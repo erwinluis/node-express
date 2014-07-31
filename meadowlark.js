0dabcc77998579d94514da87bf411e0f5394122c
Erwin Gonzalez <erwinluisgonzalez@gmail.com>
2014-07-31 15:27:53 -0400

    request headers

--- meadowlark.js
+++ meadowlark.js
@@ -23,6 +23,14 @@ app.use(function(req, res, next){
 
 // routes
 
+app.get('/headers', function(req, res){
+  res.set('Content-Type', 'text/plain');
+  var s = '';
+  for(var name in req.headers) s += name + ': ' +
+    req.headers[name] + '\n';
+  res.send(s);
+})
+
 app.get('/', function(req, res){
   res.render('home');
 });
