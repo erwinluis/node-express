780f431eda75c0e703d5a1c754d2efbe55c4ab63
Erwin Gonzalez <erwinluisgonzalez@gmail.com>
2014-08-08 11:47:06 -0400

    domain support for improved error handling.

--- meadowlark.js
+++ meadowlark.js
@@ -30,6 +30,51 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// use domains for better error handling
+app.use(function(req, res, next){
+  // create a domain for this request
+  var domain = require('domain').create();
+  // handle errors on this domain
+  domain.on('error', function(err){
+    console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+    try {
+      // failsafe shutdown in 5 seconds
+      setTimeout(function(){
+        console.error('Failsafe shutdown.');
+        process.exit(1);
+      }, 5000);
+
+      // disconnect from the cluster
+      var worker = require('cluster').worker;
+      if(worker) worker.disconnect();
+
+      // stop taking new requests
+      server.close();
+
+      try {
+        // attempt to use Express error route
+        next(err);
+      } catch(err) {
+        // if Express error route failed, try
+        // plain Node response
+        console.error('Express error mechanism failed.\n', err.stack);
+        res.statusCode = 500;
+        res.setHeader('content-type', 'text/plain');
+        res.end('Server error.');
+      }
+    } catch(err) {
+      console.error('Unable to send 500 response.\n', err.stack);
+    }
+  });
+
+  // add the request and response object to the domain
+  domain.add(req);
+  domain.add(res);
+
+  // execute the rest of the request chain in the domain
+  domain.run(next);
+});
+
 // logging
 switch(app.get('env')){
   case 'development':
@@ -98,6 +143,12 @@ function getWeatherData() {
 app.use(function(req, res, next){
   if(!res.locals.partials) res.locals.partials = {};
   res.locals.partials.weather = getWeatherData();
+  next();
+});
+
+app.use(function(req, res, next){
+  var cluster = require('cluster');
+  if(cluster.isWorker) console.log('Worker %d received request', cluster.worker.id);
   next();
 });
 
@@ -360,6 +411,12 @@ app.post('/cart/checkout', function(req, res){
   res.render('cart-thank-you', {cart: cart});
 });
 
+app.get('/epic-fail', function(req, res){
+  process.nextTick(function(){
+    throw new Error('Kaboom!');
+  });
+});
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
   res.status(404);
@@ -373,8 +430,20 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log( 'Express started in ' + app.get('env') +
-    ' mode on http://localhost:' + app.get('port') +
-    '; press Ctrl-C to terminate.' );
-});
+var server;
+
+function startServer() {
+  server = http.createServer(app).listen(app.get('port'), function(){
+    console.log( 'Express started in ' + app.get('env') +
+      ' mode on http://localhost:' + app.get('port') +
+      '; press Ctrl-C to terminate.' );
+  });
+}
+
+if(require.main === module){
+  // application run directly; start app startServer
+  startServer();
+} else {
+  //application imported as a module via "require": export function to create startServer
+  module.exports = startServer;
+}
